51662a087b94fd453bad716b6a88e0951199a8f3
wangyongqing01 <wangyongqing01@baidu.com>
2017-09-28 17:47:31 +0800

    复利计算按错

--- src/pages/shouyi/fuli.js
+++ src/pages/shouyi/fuli.js
@@ -1,14 +1,15 @@
 Page({
     data: {
         // 数据
-        money: '',
-        rate: '',
-        period: '',
-        multiple: 1,
-        periodType: '365',
+        money: 0,
+        rate: 0,
+        period: 0,
+        long: 0,
+        multiple: 10000,
+        periodType: 12,
+        show: true,
         result: 0,
         total: 0,
-        dayInterest: 0,
         // 数组
         periodTypes: [
             {
@@ -39,9 +40,58 @@ Page({
                 checked: false
             }
         ]
-
     },
-    periodTypeChange: function(e){
+    doit: function() {
+        const t = this;
+        const data = t.data;
+        let done = true;
+        ['money', 'rate', 'period', 'long'].forEach(function(id) {
+            if (!data[id]) {
+                done &&
+                    wx.showToast({
+                        title: '请填写正确字段',
+                        image: '../../image/warn.png'
+                    });
+                done = false;
+            }
+        });
+        if (done) {
+            let money = data.money * data.multiple;
+            let period = data.period;
+            let rate = data.long * (data.rate / data.periodType) / 100;
+            let result = Math.pow(1 + rate, period) * money;
+            result = parseFloat(result.toFixed(2));
+            //计算总投入
+            let interest = parseFloat((result - money).toFixed(2));
+            t.setData({
+                show: true,
+                result: result,
+                principal: money,
+                interest: interest
+            });
+        }
+    },
+    inputRate: function(e) {
+        this.setData({
+            rate: parseFloat(e.detail.value)
+        });
+    },
+    inputPeriod: function(e) {
+        this.setData({
+            period: parseInt(e.detail.value)
+        });
+    },
+    inputMoney: function(e) {
+        this.setData({
+            money: parseFloat(e.detail.value)
+        });
+    },
+    inputLong: function(e) {
+        this.setData({
+            long: e.detail.value | 0
+        });
+    },
+    periodTypeChange: function(e) {
         let radioItems = this.data.periodTypes;
         const val = e.detail.value;
         for (let i = 0, len = radioItems.length; i < len; ++i) {
@@ -49,10 +99,11 @@ Page({
         }
 
         this.setData({
+            periodType: val,
             periodTypes: radioItems
         });
     },
-    backMoneyChange: function(e) {
+    multChange: function(e) {
         let radioItems = this.data.fMultiples;
         const val = e.detail.value;
         for (let i = 0, len = radioItems.length; i < len; ++i) {
@@ -60,6 +111,7 @@ Page({
         }
 
         this.setData({
+            multiple: val,
             fMultiples: radioItems
         });
     }
